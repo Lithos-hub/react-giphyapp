eadf5f1caacad1374a0f321ae13b63c4719b2aa6
Lithos <lithos.contact@gmail.com>
2020-10-05 15:33:36 +0200

    Docs added

---
+++ docs/precache-manifest.26a7927ce7502d433afcc390a0e12d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c34268a6b0c9c7fedafe2dfb32c9f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "649008eb0b5202c40235",
+    "url": "/static/css/main.dda77e03.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649008eb0b5202c40235",
+    "url": "/static/js/main.f4ce5631.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
